4b9c55965d2adfb8e9c6fc9dd68ce6febd2e282c
Pruthviraj Chaudhari <chaudharipruthviraj888@gmail.com>
2023-06-22 08:52:36 +0530

    first commit

---
+++ js/algorithms.js
@@ -0,0 +1,502 @@
+var x;
+
+export async function bubbleSort(data, state){
+    let i, j, len = data.length;
+
+    x = d3.scaleLinear()
+        .domain([0,d3.max(data)])
+        .range([0,700]);
+
+    for (i = 0; i < len; i++)
+        drawUnselect(i);
+    
+    for (i = 0; i < len ; i++){
+        for(j = 0; j < len - i - 1; j++){
+            drawSelect(j);
+            drawSelect(j + 1);
+            await sleep(state.delay);
+
+            if (data[j] > data[j + 1]){
+                [data[j], data[j + 1]] = [data[j + 1], data[j]]
+                drawSwapSelected(j, j + 1);
+                await sleep(state.delay);
+            }
+            
+            if (!state.running) 
+                return data;
+            if (state.pause)
+                while (state.pause) 
+                    await sleep(100);
+        
+            drawUnselect(j);
+            drawUnselect(j + 1);
+        }
+        
+        drawSorted(len - 1 - i);
+    }
+    return data;
+}
+
+export async function insertionSort(data, state){ 
+    let i, j, k, key, len = data.length;
+    x = d3.scaleLinear()
+        .domain([0,d3.max(data)])
+        .range([0,700]);
+
+    for (i = 0; i < len; i++)
+        drawUnselect(i);
+
+    drawSorted(0);
+    await sleep(state.delay);
+
+    for (i = 1; i < len; i++){ 
+        key = data[i];
+        j = i - 1; 
+        drawSelect(i);
+        await sleep(state.delay);
+        
+        if (!state.running) 
+            return data;
+        if (state.pause)
+            while (state.pause) 
+                await sleep(100);
+
+        while (j >= 0 && data[j] > key){ 
+            drawAltSelect(j);
+            data[j + 1] = data[j]; 
+            j--;  
+            await sleep(state.delay);
+            
+            if (!state.running) 
+                return data;
+            if (state.pause)
+                while (state.pause) 
+                    await sleep(100);
+        } 
+        data[j + 1] = key; 
+        for (k = j + 1; k <= i; k++) 
+            drawUpdate(data[k], k);
+
+        for (k = j + 1; k <= i; k++) 
+            drawSorted(k);
+        
+        await sleep(state.delay);
+    } 
+    return data;
+} 
+
+export async function selectionSort(data, state){
+    let i, j, min_id, len = data.length;
+    x = d3.scaleLinear()
+        .domain([0,d3.max(data)])
+        .range([0,700]);
+    for (i = 0; i < len; i++)
+        drawUnselect(i);
+
+    for (i = 0; i < len; i++){
+        min_id = i;
+        drawSelect(i);
+        await sleep(state.delay);
+        for (j = i + 1; j < len; j++){
+            drawSelect(j);
+            if (data[j] < data[min_id]){
+                drawUnselect(min_id);
+                await sleep(state.delay);
+                min_id = j;
+                drawSelect(j);
+            }
+            else{
+                drawAltSelect(j);
+                if (!state.running) 
+                    return data;
+                if (state.pause)
+                    while (state.pause) 
+                        await sleep(100);
+                await sleep(state.delay);
+                drawUnselect(j);
+            }
+            if (!state.running) 
+                return data;
+            if (state.pause)
+                while (state.pause) 
+                    await sleep(100);
+            
+            
+        }
+
+        [data[i], data[min_id]] = [data[min_id], data[i]]
+        drawSwapSelected(i, min_id);
+        drawUnselect(min_id);
+        drawSorted(i);
+        await sleep(state.delay);
+    }
+    return data;
+
+}
+
+export async function mergeSort(data, state){
+    x = d3.scaleLinear()
+        .domain([0,d3.max(data)])
+        .range([0,700]);
+
+    for (let i = 0; i < data.length; i++)
+        drawUnselect(i);
+
+    await mergeSortRun(data, state);
+
+    if (state.running)
+        for (let i = 0; i < data.length; i++){
+            drawUpdate(data[i], i);
+            drawSorted(i);
+            await sleep (state.delay);
+        }
+    return data;
+}
+
+async function mergeSortRun(data, state){
+    if (data.length < 2) 
+        return data;
+
+    let mid =  Math.ceil(data.length / 2), i, il, ir, left, right;
+    left = await mergeSortRun(data.slice(0, mid), state);
+    if (left == null)
+        return null;
+    state.opt += mid;
+    right = await mergeSortRun(data.slice(mid), state);
+    if (left == null)
+        return null;
+    state.opt -= mid;
+
+    if (!state.running) 
+            return data;
+    if (state.pause)
+        while (state.pause) 
+            await sleep(100);
+    
+    for (let i = 0; i < data.length; i++)
+        drawAltSelect(state.opt + i);
+
+    for (i = 0, il = 0, ir = 0; i < (left.length + right.length); i++){
+        if (!state.running) 
+            return data;
+        if (state.pause)
+            while (state.pause) 
+                await sleep(100);
+
+        drawSelect(state.opt + i);
+
+        if (left[il] < right[ir] ){
+            data[i] = left[il];
+            await sleep(state.delay);
+            drawUpdate(data[i],state.opt + i);
+            il++;
+        }
+        else{
+            data[i] = right[ir];
+            await sleep(state.delay);
+            drawUpdate(data[i], state.opt + i);
+            ir++;
+        }
+        drawSorted(state.opt + i);
+
+      	if (il == left.length)
+            for (i++; ir < right.length; ir++, i++){
+                if (!state.running) 
+                    return data;
+                if (state.pause)
+                    while (state.pause) 
+                        await sleep(100);
+    
+                await sleep(state.delay)
+
+                data[i] = right[ir];
+                drawSelect(state.opt + i);
+                await sleep (state.delay);
+                drawUpdate(data[i], state.opt + i);
+                await sleep(state.delay);
+                drawSorted(state.opt + i);
+            }
+        if (ir == right.length)
+            for (i++; il < left.length; il++, i++){
+                if (!state.running) 
+                    return data;
+                if (state.pause)
+                    while (state.pause) 
+                        await sleep(100);
+                        
+                await sleep(state.delay)
+
+                data[i] = left[il];
+                drawSelect(state.opt + i);  
+                await sleep (state.delay); 
+                drawUpdate(data[i], state.opt + i)
+                await sleep (state.delay);
+                drawSorted(state.opt + i);
+            }
+            await sleep(state.delay)
+
+    }
+    await sleep(state.delay)
+    for (let i = 0; i < data.length; i++)
+        drawUnselect(state.opt + i);
+    
+    return data;
+}
+
+export async function quickSort(data, state){
+    x = d3.scaleLinear()
+    .domain([0,d3.max(data)])
+    .range([0,700]);
+
+    for (let i = 0; i < data.length; i++)
+        drawUnselect(i);
+
+
+    await quickSortRun(data, 0, data.length - 1, state);
+    if (state.running)
+        for (let i = 0; i < data.length; i++){
+            drawUpdate(data[i], i);
+            drawSorted(i);
+            await sleep (state.delay);
+        }
+    
+    return data;
+}
+
+async function quickSortRun(data, low, high, state){
+    if (low < high){
+        let pi = await quickSortPartition(data, low, high, state);
+        await quickSortRun(data, low, pi - 1, state);
+        await quickSortRun(data, pi, high, state);
+    }
+}
+
+async function quickSortPartition(data, low, high, state){
+    let i=low, j=high, pivot_id = Math.floor((low + high) / 2), pivot = data[pivot_id];
+    drawSelect(pivot_id);
+    await sleep(state.delay);
+    if (!state.running) 
+        return data;
+    if (state.pause)
+    while (state.pause) 
+        await sleep(100);
+    while(i <= j) {
+        while(data[i] < pivot){
+            drawAltSelect(i);
+            await sleep(state.delay);
+            if (!state.running) 
+                return data;
+            if (state.pause)
+            while (state.pause) 
+                await sleep(100);
+            drawUnselect(i);
+            i++; 
+
+        }
+        if (i != pivot_id)
+            drawSorted(i);
+        await sleep(state.delay);
+
+        while(data[j] > pivot){
+            drawAltSelect(j);
+            await sleep(state.delay);
+            if (!state.running) 
+                return data;
+            if (state.pause)
+            while (state.pause) 
+                await sleep(100);
+            drawUnselect(j);
+            j--; 
+
+        }
+        if (j != pivot_id)
+            drawSorted(j);
+        await sleep(state.delay);
+        if(i <= j) {
+            drawSwapSelected(i, j);
+            await sleep(state.delay);
+
+            [data[i], data[j]] = [data[j], data[i]];
+            i++; j--; 
+        }
+
+        if (!state.running) 
+            return data;
+        if (state.pause)
+        while (state.pause) 
+            await sleep(100);
+
+        drawUnselect(i - 1);
+        drawUnselect(j + 1);
+        if (i - 1 == pivot_id) {
+            drawUnselect(pivot_id);
+            drawSelect(j + 1);
+            pivot_id = j + 1;
+        }
+        if (j + 1 == pivot_id) {
+            drawUnselect(pivot_id);
+            drawSelect(i - 1);
+            pivot_id = i - 1; 
+        }
+
+        await sleep(state.delay);
+        if (!state.running) 
+            return data;
+        if (state.pause)
+        while (state.pause) 
+            await sleep(100);
+    }
+    drawUnselect(pivot_id);
+
+    return i;
+}
+
+export async function heapSort(data, state){
+    let n = data.length - 1;
+    
+    x = d3.scaleLinear()
+    .domain([0,d3.max(data)])
+    .range([0,700]);
+
+    for (let i = 0; i < data.length; i++)
+        drawUnselect(i);
+
+    await heapSortCreateHeap(data, state);
+    for (let i = 0; i < data.length; i++)
+        drawAltSelect(i);
+
+    await sleep(state.delay);
+
+  	while (n > 0){
+        drawSelect(0);
+        await sleep(state.delay);
+        if (!state.running) 
+            return data;
+        if (state.pause)
+            while (state.pause) 
+                await sleep(100);
+
+        drawSwapSelected(0, n);
+        drawSorted(n);
+        await sleep(state.delay);
+        if (!state.running) 
+            return data;
+        if (state.pause)
+            while (state.pause) 
+                await sleep(100);
+
+        [data[0], data[n]] = [data[n], data[0]];
+        await heapSortHeapify(data, 0, n--, state);
+    }
+    drawSorted(0);
+    return data;
+}
+
+async function heapSortCreateHeap(data, state){
+    let i = Math.floor(data.length / 2 - 1);
+    while (i >= 0){
+        await heapSortHeapify(data, i--, data.length, state);
+    }
+}
+
+
+export async function radixLSDSort(data, state){
+    let i, j, a, k, c, d, ith;
+
+    x = d3.scaleLinear()
+        .domain([0,d3.max(data)])
+        .range([0,700]);
+    
+    for (i = 0; i < data.length; i++)
+        drawUnselect(i);
+
+    k = Math.max(...data).toString().length;
+    
+
+    for (i = 0; i < k; i++){
+        a = Array.from(Array(10), () => new Array());
+
+        for (j = 0; j < data.length; j++){
+            drawSelect(j);
+            await sleep(state.delay);
+            drawUnselect(j);
+            ith = digit(data[j], i);
+          	a[isNaN(ith) ? 0 : ith].push(data[j]);   
+            if (!state.running) 
+                return data;
+            if (state.pause)
+            while (state.pause) 
+                await sleep(100);
+        }
+
+        // this is done to make inplace changes on data. Probably not the smartest way but at least it works...
+        d = new Array();
+        for (j = 0; j < a.length; j++)
+           	d = d.concat(a[j]);
+        for (j = 0; j < data.length; j++)
+            data[j] = d[j];
+
+        for (j = 0; j < data.length; j++) { 
+            drawUpdate(data[j], j);
+            if (!state.running) 
+                return data;
+            if (state.pause)
+            while (state.pause) 
+                await sleep(100);
+            if (i == k - 1)
+                drawSorted(j);
+            await sleep(state.delay);
+        }
+    }
+    return data;
+}
+
+function digit(x, i){
+    let xString = x.toString();
+    return Number(xString[xString.length - 1 - i]);
+}
+
+function sleep(ms){
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function drawSelect(i){
+    var diagram = document.getElementById("content").childNodes;
+    var elem = diagram.item(i);
+    elem.style.setProperty("background", "rgb(255, 0, 255)", null);
+}
+
+function drawAltSelect(i){
+    var diagram = document.getElementById("content").childNodes;
+    var elem = diagram.item(i);
+    elem.style.setProperty("background", "rgb(200, 200, 0)", null);
+}
+
+function drawUnselect(i, j){
+    var diagram = document.getElementById("content").childNodes;
+    var elem = diagram.item(i);
+    elem.style.setProperty("background", "#000", null);
+}
+
+
+function drawSwapSelected(i , j){
+    var diagram = document.getElementById("content").childNodes;
+    var first = diagram.item(i);
+    var second = diagram.item(j);
+    let temp = first.style.getPropertyValue("height");
+    first.style.setProperty("height", second.style.getPropertyValue("height"), null);
+    second.style.setProperty("height", temp, null);
+}
+
+function drawUpdate(val, i){
+    var diagram = document.getElementById("content").childNodes;
+    var elem = diagram.item(i);
+    elem.style.setProperty("height", x(val) + "px", null);
+}
+
+
+function drawSorted(i){
+    var diagram = document.getElementById("content").childNodes;
+    var sorted = diagram.item(i);
+    sorted.style.setProperty("background", "rgb(0, 220, 0)", null);
+}
---
+++ js/core.js
@@ -0,0 +1,223 @@
+import {bubbleSort, heapSort, insertionSort, mergeSort, quickSort, radixLSDSort, selectionSort} from './algorithms.js'
+
+
+
+var currentAlgorithm = "Bubble sort", currentArraySize = 30, currentData = [], metaFile = "../assets/meta.json", meta;
+var state = {
+    running: false,
+    pause: false,
+    delay: 10,
+    opt: 0
+};
+var algorithms = {
+    "Bubble sort": bubbleSort,
+    "Insertion sort": insertionSort,
+    "Selection sort": selectionSort,
+    "Merge sort": mergeSort,
+    "Quick sort": quickSort
+};
+
+async function loadJson(url){
+    const r = await fetch(url);
+    return r.json();
+}
+
+meta = await loadJson(metaFile);
+window.addEventListener("DOMContentLoaded", init());
+
+function shuffle(data, arraySize){
+    // Knuth shuffle
+    let i = 1, j;
+    data = Array.from({length: arraySize}, () => {return i++;})
+    i = arraySize;
+    while (i != 0){
+        j = Math.floor(Math.random() * i);
+        i--;
+        [data[i], data[j]] = [data[j], data[i]];
+    }
+
+    currentData = data;
+    redrawDiagram(data);
+}
+
+function updateCurrentAlgorithm(algorithm){
+    document.getElementById(currentAlgorithm).setAttribute("active", "false");
+    currentAlgorithm = algorithm;
+    document.getElementById(currentAlgorithm).setAttribute("active", "true");
+    
+    let algorithmMeta = meta[currentAlgorithm];
+    for (const [key, value] of Object.entries(algorithmMeta["complexity"])){
+        let elem = document.getElementById(key);
+        elem.innerHTML = transformMeta(value["val"]);
+        elem.setAttribute("class", "cell " + value["qual"]);
+        
+    }
+    document.getElementById("source-code").href = algorithmMeta["source-code"];
+}
+
+function transformMeta(val){
+    switch (val){ 
+        case ("1"):
+            return "<i>O(1)</i>";
+
+        case ("n"):
+            return "<i>O(n)</i>";
+        
+        case ("nk"):
+            return "<i>O(nk)</i>";
+
+        case ("n+k"):
+            return "<i>O(n+k)</i>";
+
+        case ("n^2"):
+            return "<i>O(</i><i>n</i><sup>2</sup><i>)</i>";
+        
+        case ("log_n"):
+            return "<i>O(</i>log <i>n</i><i>)</i>";
+
+        case ("n_log_n"):
+            return "<i>O(</i><i>n</i> log <i>n</i><i>)</i>";
+
+        default:
+            return val;
+    }
+}
+
+function updateCurrentDelay(delay){
+    state.delay = delay;
+    document.getElementById("delayRangeValue").innerHTML = delay + " ms";
+}
+
+function updateCurrentArraySize(arraySize){
+    currentArraySize = arraySize;
+    document.getElementById("arraySizeRangeValue").innerHTML = arraySize;
+}
+
+function sortingStart(s){
+    s.running = true;
+    s.pause = false;
+
+    document.getElementsByClassName("start")[0].innerHTML = "Pause";
+    document.getElementsByClassName("shuffle")[0].innerHTML = "Stop & Shuffle";
+}
+
+function sortingStop(s){
+    s.running = false;
+    s.pause = false;
+
+    document.getElementsByClassName("start")[0].innerHTML = "Start";
+    document.getElementsByClassName("shuffle")[0].innerHTML = "Shuffle";
+}
+
+function sortingPause(s){
+    s.running = true;
+    s.pause = true;
+
+    document.getElementsByClassName("start")[0].innerHTML = "Continue";
+}
+
+function initAlgorithmOptions(){
+    var algorithmsList = document.getElementById("algorithmSubmenu");
+    for (const [key, value] of Object.entries(algorithms)) {
+        var newElem = document.createElement("li");
+        newElem.innerHTML = "<a class=\"algorithmSubmenuOption\" href=\"#\" id =\"" + key + "\">" + key + "</a>";
+        algorithmsList.appendChild(newElem);
+    }
+}
+
+function redrawDiagram(data){
+
+    var element = document.getElementById("content");
+    if (element.firstChild)
+        d3.select(".chartHTML")
+            .selectAll("div")
+            .style("height", "0px")
+            .style("margin", "0px")
+            .style("padding", "0px")
+            .style("border", "0px");
+
+        function divCleaner()
+        {
+            while (element.firstChild)
+                element.removeChild(element.firstChild);   
+        }
+
+        divCleaner();
+
+        var x = d3.scaleLinear()
+            .domain([0,d3.max(data)])
+            .range([0,700]);
+
+        d3.select(".chartHTML")
+            .selectAll("div")
+            .data(data)
+            .enter().append("div")
+            .transition(3000)
+            .style("height", function(d){
+                return x(d) + "px";
+            })
+            .style("background", function(){
+                return "#010101";
+            });
+            
+}
+
+function init(){
+    initAlgorithmOptions(); 
+    updateCurrentAlgorithm(currentAlgorithm);
+    updateCurrentDelay(state.delay);
+    updateCurrentArraySize(currentArraySize);
+    shuffle(currentData, currentArraySize);
+
+    $(document).ready(function(){
+        $('#sidebarCollapse').on('click', function(){
+            $('#sidebar').toggleClass('active');
+        });
+    });
+
+    $("a.algorithmSubmenuOption").click(function(){
+        sortingStop(state);
+        updateCurrentAlgorithm($(this).text());
+    });
+    $('input#delayRange').val(state.delay);
+    $('input#delayRange').on('input change', function(){
+        updateCurrentDelay($(this).val());
+    });
+
+    $('input#arraySizeRange').val(currentArraySize);
+    $('input#arraySizeRange').on('input change', function(){
+        updateCurrentArraySize($(this).val());
+    });
+
+    // $('input#arraySizeRange').on('change', function(){
+    //     if (!state.running)
+    //         shuffle(currentData, currentArraySize);
+    // });
+
+    $('a.shuffle').click(function(){
+        sortingStop(state);
+        shuffle(currentData, currentArraySize);
+    });
+
+    $('a.start').click(function(){
+        if (state.running & !state.pause){
+            sortingPause(state);
+            return;
+        }
+        if (state.running & state.pause){
+            sortingStart(state);
+            return;
+        }
+
+        if (currentArraySize != currentData.length)
+            shuffle(currentData, currentArraySize);
+
+        sortingStart(state);
+        state.opt = 0;
+        algorithms[currentAlgorithm](currentData, state).then(function() {sortingStop(state)});
+    });
+
+}
+
+
+
